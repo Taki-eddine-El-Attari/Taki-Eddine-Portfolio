91a54909fd60ff1a833217e8f0e84163e4bc79ae
Taki Eddine <takieddineelattari@gmail.com>
2025-08-03 18:43:33 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import NavBar from "./components/NavBar";
+import Experience from "./sections/Experience";
+import FeatureCards from "./sections/FeatureCards";
+import Hero from "./sections/Hero";
+import LogoShowcase from "./sections/LogoShowcase";
+import AppShowcase from "./sections/ShowcaseSection";
+
+const App = () => {
+  return (
+    <>
+    <NavBar/>
+    <Hero />
+    <AppShowcase />
+    <LogoShowcase />
+    <FeatureCards />
+    <Experience />
+    </>
+  );
+}
+
+export default App;
